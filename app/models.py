ff279cf85e01b10c1c70d288f5d76c360345ecdc
teilon <teiloncw@gmail.com>
2018-10-03 08:18:29 +0600

    init

---
+++ app/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+
+# class ProductSet(models.Model):
+#     m1 = models.IntegerField(default=0)
+#     m2 = models.IntegerField(default=0)
+#     m3 = models.IntegerField(default=0)
+#     m4 = models.IntegerField(default=0)
+#     m5 = models.IntegerField(default=0)
+#     m6 = models.IntegerField(default=0)
+#     m7 = models.IntegerField(default=0)
+#     m8 = models.IntegerField(default=0)
+#     m9 = models.IntegerField(default=0)
+#     m10 = models.IntegerField(default=0)
+#     w1 = models.IntegerField(default=0)
+#     w2 = models.IntegerField(default=0)
+#     w3 = models.IntegerField(default=0)
+#     w4 = models.IntegerField(default=0)
+#     w5 = models.IntegerField(default=0)
+#     w6 = models.IntegerField(default=0)
+#     w7 = models.IntegerField(default=0)
+#     w8 = models.IntegerField(default=0)
+#     w9 = models.IntegerField(default=0)
+#     w10 = models.IntegerField(default=0)
+#
+#
+# class Client(models.Model):
+#     CHOISE_OF_FORM = {
+#         'DL': 'diler',
+#         'MT': 'market',
+#         'PS': 'person'
+#     }
+#
+#     name = models.CharField(max_length=50)
+#     form = models.CharField(choices=CHOISE_OF_FORM)
+#
+#
+# class TransactionStatus(models.Model):
+#     CHOISE_OF_TRANSACTION_STATUS = {
+#         'AC': 'accept',
+#         'RT': 'return'
+#     }
+#
+#     status = models.CharField(choices=CHOISE_OF_TRANSACTION_STATUS)
+#
+#
+# class Transaction(models.Model):
+#     status = models.ForeignKey('TransactionStatus', on_delete=models.CASCADE)
+#     client = models.ForeignKey('Client', on_delete=models.CASCADE)
+#     product_set = models.ForeignKey('ProductSet', on_delete=models.CASCADE)
+#     # creation_date
+#
+#
+# class Storage(models.Model):
+#     product_set = models.ForeignKey('ProductSet', on_delete=models.CASCADE)
+#     # updating_date
+
+
