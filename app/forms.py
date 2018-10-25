997242eb5be754e6b1fe06b3743b3adc2ab46f82
teilon <teiloncw@gmail.com>
2018-10-25 09:31:13 +0600

    add transaction new page

--- app/forms.py
+++ app/forms.py
@@ -14,7 +14,8 @@ class NewTransactionForm(forms.ModelForm):
 
     class Meta:
         model = ProductSet
-        fields = ('m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10')
-        widgets = {
-            'myfield': forms.TextInput(attrs={'class': 'myfieldclass'}),
-        }
+        fields = ('m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10',
+                  'w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7', 'w8', 'w9', 'w10')
+        # widgets = {
+        #     'myfield': forms.TextInput(attrs={'class': 'myfieldclass'}),
+        # }
